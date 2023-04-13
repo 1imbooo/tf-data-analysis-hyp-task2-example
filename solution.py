4fba466485a2603a65efc3bd538406497dd029fb
1imbooo <130186620+1imbooo@users.noreply.github.com>
2023-04-13 21:12:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import ks_2samp, cramervonmises_2samp
 
 
 chat_id = 661128504 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.03
-    ks_statistic, ks_pvalue = ks_2samp(x, y)
+    cramer = cramervonmises_2samp(x, y)
     
-    if ks_pvalue < alpha:
+    if cramer.pvalue < alpha:
         return True
     else:
         return False
